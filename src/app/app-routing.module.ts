c323d338e8a84d489ecc6260e851c8445c0954c9
joshdavis <jldavis33@yahoo.com>
2021-11-23 14:26:10 -0600

    Start hero-list and crisis-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
