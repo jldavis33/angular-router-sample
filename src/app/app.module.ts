ea883c5f06b0ac63abf11f87489d4d933e3b44db
joshdavis <jldavis33@yahoo.com>
2021-11-24 15:36:04 -0600

    Migrating Tour of Hero file contents, updating imports, removing BS4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroListComponent,
     PageNotFoundComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
