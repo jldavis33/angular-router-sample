12329034b681c45503f120b9eb28eb8ad9a8adc5
joshdavis <jldavis33@yahoo.com>
2021-11-24 17:05:12 -0600

    fix hero service

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -5,19 +5,27 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from '../message.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+    constructor(private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
-  }
+    getHeroes(): Observable<Hero[]> {
+        const heroes = of(HEROES);
+        this.messageService.add('HeroService: fetched heroes');
+        return heroes;
+    }
+
+    getHero(id: number | string) {
+        return this.getHeroes().pipe(
+            // (+) before `id` turns the string into a number
+            map((heroes: Hero[]) => heroes.find(hero => hero.id === +id)!)
+        );
+    }
 }
 
 
